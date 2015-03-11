3902d006f680ef211b1f906e5c10465832ec65a5
Michelle Tan <michelletanjm@gmail.com>
2015-03-11 11:51:08 +0800

    Added static form values for New Project page

--- BioLifeTracker/BioLifeTracker/ProjectFormViewController.swift
+++ BioLifeTracker/BioLifeTracker/ProjectFormViewController.swift
@@ -8,18 +8,46 @@
 
 import UIKit
 
-class ProjectFormViewController: UITableViewController, UITableViewDataSource, UITableViewDelegate {
+class ProjectFormViewController: UITableViewController {
+
+    @IBOutlet weak var textFieldTitle: UITextField!
+    @IBOutlet weak var textFieldAnimal: UITextField!
+    @IBOutlet weak var textFieldEthogram: UITextField!
+
+    @IBOutlet weak var btnCreateEthogram: UIButton!
+    @IBOutlet weak var btnSelectEthogram: UIButton!
     
-    let table = UITableViewController()
-    
-    let cellReuseIdentifier = "ProjectFormTableCell"
-    
-    var isTableEditable = false
-    
-    
-    var cellTypes: [Constants.CellType] = []
+    let messageNoEthograms = "You have no ethograms saved"
+    let messageNoEthogramsSelected = "Select an ethogram"
+    var selectedEthogram: Ethogram? = nil
     
     override func viewDidLoad() {
-        
+        super.viewDidLoad()
+        toggleVisibilityOfViews()
+    }
+    
+    func toggleVisibilityOfViews() {
+        if Data.ethograms.isEmpty {
+            btnCreateEthogram.hidden = false
+            btnSelectEthogram.hidden = true
+            textFieldEthogram.text = messageNoEthograms
+        } else {
+            btnCreateEthogram.hidden = true
+            btnSelectEthogram.hidden = false
+            
+            if selectedEthogram == nil {
+                textFieldEthogram.text = messageNoEthogramsSelected
+            } else {
+                textFieldEthogram.text = selectedEthogram!.name
+            }
+        }
+    }
+    
+    @IBAction func btnCreateEthogramPressed(sender: UIButton) {
+        // Segue to create ethogram with this project's id
+    }
+    
+    @IBAction func btnSelectEthogramPressed(sender: UIButton) {
+        // Pop up a table view picker (with xib?)
     }
 }
