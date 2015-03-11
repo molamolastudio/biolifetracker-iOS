7c7e00b54bfb97f91c00295879c717d0c258a939
Michelle Tan <michelletanjm@gmail.com>
2015-03-11 12:42:15 +0800

    Added New Ethogram page

---
+++ BioLifeTracker/BioLifeTracker/EthogramFormViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EthogramFormViewController.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 11/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class EthogramFormViewController: UITableViewController {
+    
+    let tagCellLabel = 200
+    let tagCellTextField = 201
+    let tagCellFullTextField = 202
+    
+    let cellReuseLabelAndText = "LabelWithTextField"
+    let cellReuseTextOnly = "TextFieldOnly"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
