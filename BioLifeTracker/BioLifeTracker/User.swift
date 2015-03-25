8761b581f1ac9ff9145fa960f24054c5f8cd762a
Nicholette <nekonekonik@gmail.com>
2015-03-25 16:56:08 +0800

    Edited attributes for user class.

--- BioLifeTracker/BioLifeTracker/User.swift
+++ BioLifeTracker/BioLifeTracker/User.swift
@@ -9,9 +9,17 @@
 import Foundation
 
 class User: NSObject {
+    var id: String          // Use email or object id
     var name: String
     
-    init(name: String) {
+    //        println("User: \(user)")
+    //        println("User ID: \(user.objectID)")
+    //        println("User Name: \(user.name)")
+    //        var userEmail = user.objectForKey("email") as String
+    //        println("User Email: \(userEmail)")
+    
+    init(id: String, name: String) {
+        self.id = id
         self.name = name
     }
 }
