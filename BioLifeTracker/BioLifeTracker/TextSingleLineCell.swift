ac7f88055988719d29732069449f1ccc13bca367
Michelle Tan <michelletanjm@gmail.com>
2015-03-30 18:00:36 +0800

    Added xibs for Text and Boolean cells

---
+++ BioLifeTracker/BioLifeTracker/TextSingleLineCell.swift
@@ -0,0 +1,13 @@
+//
+//  TextSingleLineCell.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 30/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class TextSingleLineCell: UITableViewCell {
+    
+}
