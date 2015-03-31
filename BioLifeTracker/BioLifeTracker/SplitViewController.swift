90f069c2f8a5cebc3403324e3c551cfa09eac81f
Michelle Tan <michelletanjm@gmail.com>
2015-03-31 15:40:41 +0800

    Added Super and Split view controllers

---
+++ BioLifeTracker/BioLifeTracker/SplitViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SplitViewController.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 31/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class SplitViewController: UISplitViewController {
+    override func viewDidLoad() {
+        println("split view loaded")
+    }
+}
---
+++ BioLifeTracker/BioLifeTracker/SuperController.swift
@@ -0,0 +1,21 @@
+//
+//  SuperController.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 31/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+//  This class controls all controllers of the application, and
+//  swaps the presented view controller if needed.
+//
+
+import UIKit
+
+class SuperController: UIViewController {
+    
+    let splitVC = SplitViewController()
+    
+    override func viewDidLoad() {
+        
+    }
+}
