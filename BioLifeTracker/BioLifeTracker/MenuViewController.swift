e620963f35ba0d4ff6f7da453777e198294692c7
Michelle Tan <michelletanjm@gmail.com>
2015-03-31 17:09:40 +0800

    Added MenuViewController

---
+++ BioLifeTracker/BioLifeTracker/MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 31/3/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UITableViewController, UITableViewDelegate, UITableViewDataSource {
+    override func viewDidLoad() {
+        
+    }
+}
