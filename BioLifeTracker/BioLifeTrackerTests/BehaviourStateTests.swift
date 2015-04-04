ebbcf07eb14910ade4d40b05d59de44112b7a6b0
Nicholette <nekonekonik@gmail.com>
2015-04-05 01:53:21 +0800

    Did testing for the data models Session and BehaviourState.

--- BioLifeTracker/BioLifeTrackerTests/BehaviourStateTests.swift
+++ BioLifeTracker/BioLifeTrackerTests/BehaviourStateTests.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+import XCTest
+
+class BehaviourStateTests: XCTestCase {
+    var state = BehaviourState(name: "Feeding", information: "Small claws bringing food to mouth")
+    
+    func testInitialization() {
+        XCTAssert(state.name == "Feeding", "BehaviourState not initialised properly")
+        XCTAssert(state.information == "Small claws bringing food to mouth", "BehaviourState not initialised properly")
+        XCTAssert(state.photoUrls.count == 0, "BehaviourState not initialised properly")
+    }
+    
+    func testUpdates() {
+        XCTAssert(state.name == "Feeding", "BehaviourState not initialised properly")
+        state.updateName("Fighting")
+        XCTAssert(state.name == "Fighting", "BehaviourState not initialised properly")
+        
+        XCTAssert(state.information == "Small claws bringing food to mouth", "BehaviourState not initialised properly")
+        state.updateInformation("Engagement of large clawa with another crab")
+        XCTAssert(state.information == "Engagement of large clawa with another crab", "BehaviourState not initialised properly")
+        
+        // Test update photo
+        
+        XCTAssert(state.photoUrls.count == 0, "BehaviourState not initialised properly")
+        
+        state.addPhotoUrl("www.photo1.com")
+        XCTAssert(state.photoUrls[0] == "www.photo1.com", "Individual not initialised properly")
+        
+        state.addPhotoUrl("www.photo2.com")
+        XCTAssert(state.photoUrls[1] == "www.photo2.com", "Individual not initialised properly")
+        
+        state.removePhotoUrlAtIndex(0)
+        XCTAssert(state.photoUrls[0] == "www.photo2.com", "Individual not initialised properly")
+    }
+
+}
--- BioLifeTracker/BioLifeTrackerTests/SessionTests.swift
+++ BioLifeTracker/BioLifeTrackerTests/SessionTests.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import XCTest
+
+class SessionTests: XCTestCase {
+    
+    func testInitialization() {
+        let state1 = BehaviourState(name: "Feeding", information: "Small claws bringing food to mouth")
+        let state2 = BehaviourState(name: "Fighting", information: "Engagement of large clawa with another crab")
+        var ethogram = Ethogram(name: "Fiddler Crabs")
+        ethogram.addBehaviourState(state1)
+        ethogram.addBehaviourState(state2)
+        
+        let project = Project(name: "A Day in a Fiddler Crab life", ethogram: ethogram)
+        
+        var session = Session(project: project, type: SessionType.Scan)
+        
+        XCTAssert(session.project.name == "A Day in a Fiddler Crab life", "Session not initialised properly")
+        XCTAssert(session.type == SessionType.Scan, "Session not initialised properly")
+        XCTAssert(session.observations.count == 0, "Session not initialised properly")
+        XCTAssert(session.individuals.count == 0, "Session not initialised properly")
+    }
+    
+    func testUpdates() {
+        let state1 = BehaviourState(name: "Feeding", information: "Small claws bringing food to mouth")
+        let state2 = BehaviourState(name: "Fighting", information: "Engagement of large clawa with another crab")
+        var ethogram = Ethogram(name: "Fiddler Crabs")
+        ethogram.addBehaviourState(state1)
+        ethogram.addBehaviourState(state2)
+        
+        let project = Project(name: "A Day in a Fiddler Crab life", ethogram: ethogram)
+        var session = Session(project: project, type: SessionType.Scan)
+        
+        let individual = Individual(label: "M1")
+        let observation1 = Observation(session: session, individual: individual, state: state1, timestamp: NSDate(), creator: UserAuthService.sharedInstance.user, information: "Eating vigourously")
+        
+        session.addObservation([observation1])
+        XCTAssert(session.observations[0].information == "Eating vigourously", "Session not updated properly")
+        
+        let observation2 = Observation(session: session, individual: individual, state: state1, timestamp: NSDate(), creator: UserAuthService.sharedInstance.user, information: "Picking up sand")
+        session.updateObservation(0, updatedObservation: observation2)
+        XCTAssert(session.observations[0].information == "Picking up sand", "Session not updated properly")
+        
+        session.deleteObservations([0])
+        XCTAssert(session.observations.count == 0, "Session not updated properly")
+        
+        
+        let individual1 = Individual(label: "M2")
+        session.addIndividuals([individual1])
+        XCTAssert(session.individuals[0].label == "M2", "Session not updated properly")
+        
+        let individual2 = Individual(label: "M3")
+        session.updateIndividual(0, updatedIndividual: individual2)
+        XCTAssert(session.individuals[0].label == "M3", "Session not updated properly")
+        
+        session.deleteIndividuals([0])
+        XCTAssert(session.individuals.count == 0, "Session not updated properly")
+    }
+}
