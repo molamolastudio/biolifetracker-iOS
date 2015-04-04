8390e2331d4f14d7c09075f7c22e9c009100f8b6
Nicholette <nekonekonik@gmail.com>
2015-04-04 16:21:31 +0800

    Removed framework warning.

---
+++ BioLifeTracker/BioLifeTrackerTests/ProjectTests.swift
@@ -0,0 +1,9 @@
+//
+//  ProjectStorageTest.swift
+//  BioLifeTracker
+//
+//  Created by Li Jia'En, Nicholette on 3/4/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import Foundation
