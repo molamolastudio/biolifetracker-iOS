6a00f4fc1e67d1755f8a9928807314e5b1c92a50
Nicholette <nekonekonik@gmail.com>
2015-04-05 11:37:19 +0800

    Finished testing for project data model.

--- BioLifeTracker/BioLifeTrackerTests/ProjectTests.swift
+++ BioLifeTracker/BioLifeTrackerTests/ProjectTests.swift
@@ -105,4 +105,132 @@ class ProjectTests: XCTestCase {
         XCTAssert(retrieved?.ethogram.behaviourStates[0].information == "Small claws bringing food to mouth", "BS in Ethogram not saved")
     }
     
+    func testInitialization() {
+        ethogram.addBehaviourState(state1)
+        var project = Project(name: "A Day in a Fiddler Crab life", ethogram: ethogram)
+        
+        XCTAssert(project.name == "A Day in a Fiddler Crab life", "Project not initialized properly")
+        XCTAssert(project.ethogram.name == "Fiddler Crabs", "Project not initialized properly")
+        XCTAssert(ethogram.behaviourStates.count == 1, "Ethogram not initialized properly")
+        
+        var containsAdmin = false
+        for admin in project.admins {
+            if admin.toString() == UserAuthService.sharedInstance.user.toString() {
+                containsAdmin = true
+            }
+        }
+        XCTAssert(containsAdmin == true, "Project not initialised properly")
+        
+        var containsMember = false
+        for member in project.members {
+            if member.toString() == UserAuthService.sharedInstance.user.toString() {
+                containsMember = true
+            }
+        }
+        XCTAssert(containsMember == true, "Project not initialised properly")
+    }
+    
+    func testUpdating() {
+        ethogram.addBehaviourState(state1)
+        var project = Project(name: "A Day in a Fiddler Crab life", ethogram: ethogram)
+        
+        XCTAssert(project.name == "A Day in a Fiddler Crab life", "Project not updated properly")
+        project.updateName("A morning of a Fiddler Crab Life")
+        XCTAssert(project.name == "A morning of a Fiddler Crab Life", "Project not updated properly")
+        
+        ethogram = Ethogram(name: "Red Fiddler Crabs")
+        ethogram.addBehaviourState(state2)
+        project.updateEthogram(ethogram)
+        
+        XCTAssert(project.ethogram.name == "Red Fiddler Crabs", "Project not updated properly")
+        XCTAssert(project.ethogram.behaviourStates[0].name == "Fighting", "Project not updated properly")
+
+        
+        project.addAdmins([user2])
+        project.addMembers([user3])
+        
+        var containsAdmin = false
+        for admin in project.admins {
+            if admin.toString() == "The Hulk" {
+                containsAdmin = true
+            }
+        }
+        XCTAssert(containsAdmin == true, "Project not updated properly")
+        
+        containsAdmin = false
+        for admin in project.admins {
+            if admin.toString() == "Black Power Ranger" {
+                containsAdmin = true
+            }
+        }
+        XCTAssert(containsAdmin == false, "Project not updated properly")
+        
+        var containsMember = false
+        for member in project.members {
+            if member.toString() == "The Hulk" {
+                containsMember = true
+            }
+        }
+        XCTAssert(containsMember == true, "Project not updated properly")
+        
+        containsMember = false
+        for member in project.members {
+            if member.toString() == "Black Power Ranger" {
+                containsMember = true
+            }
+        }
+        XCTAssert(containsMember == true, "Project not updated properly")
+        
+        project.removeAdmins([1])
+        project.removeMembers([2])
+        
+        containsAdmin = false
+        for admin in project.admins {
+            if admin.toString() == "The Hulk" {
+                containsAdmin = true
+            }
+        }
+        XCTAssert(containsAdmin == false, "Project not updated properly")
+        
+        
+        containsMember = false
+        for member in project.members {
+            if member.toString() == "The Hulk" {
+                containsMember = true
+            }
+        }
+        XCTAssert(containsMember == true, "Project not updated properly")
+        
+        containsMember = false
+        for member in project.members {
+            if member.toString() == "Black Power Ranger" {
+                containsMember = true
+            }
+        }
+        XCTAssert(containsMember == false, "Project not updated properly")
+        
+        
+        var session1 = Session(project: project, type: SessionType.Scan)
+        project.addSessions([session1])
+        XCTAssert(project.sessions[0].type == SessionType.Scan, "Project not updated properly")
+        
+        var session2 = Session(project: project, type: SessionType.Focal)
+        project.updateSession(0, updatedSession: session2)
+        XCTAssert(project.sessions[0].type == SessionType.Focal, "Project not updated properly")
+        
+        project.removeSessions([0])
+        XCTAssert(project.sessions.count == 0, "Project not updated properly")
+        
+        
+        let individual1 = Individual(label: "M2")
+        project.addIndividuals([individual1])
+        XCTAssert(project.individuals[0].label == "M2", "Session not updated properly")
+        
+        let individual2 = Individual(label: "M3")
+        project.updateIndividual(0, updatedIndividual: individual2)
+        XCTAssert(project.individuals[0].label == "M3", "Session not updated properly")
+        
+        project.removeIndividuals([0])
+        XCTAssert(project.individuals.count == 0, "Session not updated properly")
+    }
 }
