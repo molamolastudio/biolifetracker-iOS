b5907fd4a553072c6565448556412399ff36ef25
Nicholette <nekonekonik@gmail.com>
2015-04-20 00:39:52 +0800

    Added saveToArchives for methods in EthogramMng and ProjectMng

--- BioLifeTracker/BioLifeTracker/EthogramManager.swift
+++ BioLifeTracker/BioLifeTracker/EthogramManager.swift
@@ -27,21 +27,25 @@ class EthogramManager: NSObject, Storable {
     
     func updateEthograms(ethograms: [Ethogram]) {
         self._ethograms = ethograms
+        saveToArchives()
     }
     
     func addEthogram(ethogram: Ethogram) {
         self._ethograms.append(ethogram)
+        saveToArchives()
     }
     
     func updateEthogram(index: Int, ethogram: Ethogram) {
         self._ethograms.removeAtIndex(index)
         self._ethograms.insert(ethogram, atIndex: index)
+        saveToArchives()
     }
     
     func removeEthograms(indexes: [Int]) {
         for index in indexes {
             self._ethograms.removeAtIndex(index)
         }
+        saveToArchives()
     }
     
     // For testing.
--- BioLifeTracker/BioLifeTracker/ProjectManager.swift
+++ BioLifeTracker/BioLifeTracker/ProjectManager.swift
@@ -27,10 +27,12 @@ class ProjectManager: NSObject, Storable {
     
     func updateProjects(projects: [Project]) {
         self._projects = projects
+        saveToArchives()
     }
     
     func addProject(project: Project) {
         self._projects.append(project)
+        saveToArchives()
     }
     
     func updateProject(index: Int, project: Project) {
@@ -50,6 +52,7 @@ class ProjectManager: NSObject, Storable {
             }
             self._projects.removeAtIndex(index)
         }
+        saveToArchives()
     }
     
     // For testing.
