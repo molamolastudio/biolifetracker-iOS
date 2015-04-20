223b056be72c6d22fe96014e16f2b0ef08a40398
Michelle Tan <michelletanjm@gmail.com>
2015-04-20 21:07:32 +0800

    Added ProjectFormVC

---
+++ BioLifeTracker/BioLifeTracker/MemberCell.swift
@@ -0,0 +1,15 @@
+//
+//  MemberCell.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 20/4/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class MemberCell: FormCell {
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var adminLabel: UILabel!
+}
---
+++ BioLifeTracker/BioLifeTracker/MemberPickerViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MemberPickerViewController.swift
+//  BioLifeTracker
+//
+//  Created by Michelle Tan on 20/4/15.
+//  Copyright (c) 2015 Mola Mola Studios. All rights reserved.
+//
+
+import UIKit
+
+class MemberPickerViewController: UIViewController {
+    
+}
